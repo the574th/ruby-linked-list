a508510a7a2a824438f9e9436f2ece5e20240514
“the574th” <sufyazman@gmail.com>
2019-01-29 13:10:06 +0800

    cash solutiion

--- exercise.rb
+++ exercise.rb
@@ -29,3 +29,51 @@ end
 node1 = Node.new('what')
 
 puts node1.value
+
+# cash solution
+# class Node
+#   attr_accessor :value, :next
+
+#   def initialize(value)
+#     @value = value
+#     @next = nil
+#   end
+# end
+
+
+# class LinkedList
+#     attr_accessor :head
+#   def initialize(val)
+#     @head = Node.new(val)
+#   end
+
+#   def add(val)
+#     current = @head
+#     while current.next != nil
+#       current = current.next
+#     end
+#     current.next = Node.new(val)
+#   end
+
+#   def return_list
+#     puts @head.val
+#     current = @head
+#     while current.next != nil
+#       current = current.next
+#       puts current.val
+#     end
+#   end
+# end
+
+
+
+# list = LinkedList.new('hi')
+
+# puts list.head.val
+
+# list.add(2)
+# list.add(3)
+# list.add(4)
+
+
+# list.return_list
